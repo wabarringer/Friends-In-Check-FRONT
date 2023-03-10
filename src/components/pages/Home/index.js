1f225fd2b28c498f5c1e49f907a7f85810ac3ba4
dejesusf <dejesusf@uw.edu>
2023-03-10 00:28:47 -0800

    updates to homepage

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import "../Home/style.css";
 
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1>Home</h1>
+      <div className="column">
+        <div className="left">
+          BLOCK
+        </div>
+        <div className="middle">
+          BLOCK
+        </div>
+        <div className="right">
+          <div className="container" id="chatWindow">
+            Textbox
+          </div>
+          <div className="container" id="chatBox">
+            Text input
+          </div>
+        </div>
       </div>
+      
     </main>
   );
 }
