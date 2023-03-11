a1123314e3501dbf6280fb74e8ae51cfcc3e06ff
dejesusf <dejesusf@uw.edu>
2023-03-10 17:12:51 -0800

    more basic styling added to home page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -16,12 +16,12 @@ const Home = () => {
   return (
     <section>
       <div className="column">
-        <div className="component">
+        <div className="box">
           <Link to={`/room/${roomId}`}>
           <button onClick={handleHost}>Host a Room</button>
         </Link>
         </div>
-        <div className="component">
+        <div className="box">
           <button onClick={() => console.log("Join a Room")}>Join a Room</button>
         </div>
       </div>  
