1e43b0b41ef0e66d6e2d0e8e7e3633e47e58f924
William <wabarringer@gmail.com>
2023-03-10 18:04:28 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/pages/Login/index";
@@ -10,15 +10,26 @@ import Profile from "./components/pages/Profile/index";
 import Friends from "./components/pages/Friends/index";
 
 function App() {
+  const [roomId, setRoomId] = useState("");
+
+  useEffect(() => {
+    handleHost();
+  }, []);
+
+  const handleHost = () => {
+    const genId = Math.floor(Math.random() * 100000);
+    setRoomId(genId);
+  };
+
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home roomId={roomId} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/room/:roomId" element={<Room />} />
+          <Route path={`/room/${roomId}`} element={<Room />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/friends" element={<Friends />} />
         </Routes>
