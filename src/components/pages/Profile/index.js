7fe3ea0550ad4924aa6d04aea84fac542f0f020d
Brady Collier <bradycollier.123@gmail.com>
2023-03-12 00:45:13 -0800

    cloudinary functionality

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,16 +1,68 @@
 import React, { useState } from "react";
+import Axios from "axios";
 import "./style.css";
 
 const Profile = () => {
+  const [imageSelected, setImageSelected] = useState("");
+  const [publicPhotoUrl, setPublicPhotoUrl] = useState("");
+
+  const uploadImage = () => {
+    const formData = new FormData();
+    formData.append("file", imageSelected);
+    formData.append("upload_preset", "xqkzjxyi");
+
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/dkzenaz20/image/upload",
+      formData
+    )
+      .then((response) => {
+        console.log(response);
+        setPublicPhotoUrl(response.data.secure_url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
+    // <div>
+    //   <input
+    //     type="file"
+    //     onChange={(event) => {
+    //       setImageSelected(event.target.files[0]);
+    //     }}
+    //   />
+    //   <button onClick={uploadImage}>Save</button>
+    //   {publicPhotoUrl && (
+    //     <img src={publicPhotoUrl} alt="Uploaded image" />
+    //   )}
+    // </div>
     <div className="column">
+          <div>
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageSelected(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadImage}>Save</button>
+      {/* {publicPhotoUrl
+       && (
+        <img src={publicPhotoUrl} alt="Uploaded image" />
+      )
+      } */}
+    </div>
       <div className="left">
         <div id="profileSection">
           <div>
             <h3>USERNAME</h3>
             <div id="avatar">
-              IMAGE
-            </div>
+                {publicPhotoUrl
+                && (
+                  <img src={publicPhotoUrl} alt="Uploaded image" />
+                )
+      }            
+      </div>
           </div>
           
           <div id="userBio">
@@ -27,6 +79,36 @@ const Profile = () => {
       </div>
     </div>
   );
-};
+}
+
+// const Profile = () => {
+//   return (
+//     <div className="column">
+//       <div className="left">
+//         <div id="profileSection">
+//           <div>
+//             <h3>USERNAME</h3>
+//             <div id="avatar">
+//               IMAGE
+//             </div>
+//           </div>
+          
+//           <div id="userBio">
+//             USER BIO
+//           </div>
+//         </div>
+
+//       </div>
+
+//       <div className="right">
+//         <div id="feedBody">
+//           STATS FEED
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
 
 export default Profile;
