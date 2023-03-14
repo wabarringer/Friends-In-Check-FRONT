cf9882b2a920593d0097d0b887b62bcd0e006b06
William <wabarringer@gmail.com>
2023-03-14 14:31:28 -0700

    update

--- src/components/pages/Friends/index.js
+++ src/components/pages/Friends/index.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "../Friends/style.css";
 import MultiPlayerGame from "../../chess/MultiPlayerGame";
 
-return (
-  <div>
-    <h2>No friends here</h2>
-  </div>
-);
+const Friends = () => {
+  return (
+    <div>
+      <h2>No friends here</h2>
+    </div>
+  );
+};
 
 export default Friends;
