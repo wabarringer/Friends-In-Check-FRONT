acd6028414430b2f64d0821a40f0578e4b6c2de1
dejesusf <dejesusf@uw.edu>
2023-03-14 15:36:51 -0700

    updates

--- src/components/chess/Game.js
+++ src/components/chess/Game.js
@@ -21,15 +21,17 @@ export default function Game({ playerId, squares, gameStateSetter }) {
 
   return (
     <div>
-      <div>{playerId}</div>
-      <BoardRow start={0} end={8} squares={gameState}></BoardRow>
-      <BoardRow start={8} end={16} squares={gameState}></BoardRow>
-      <BoardRow start={16} end={24} squares={gameState}></BoardRow>
-      <BoardRow start={24} end={32} squares={gameState}></BoardRow>
-      <BoardRow start={32} end={40} squares={gameState}></BoardRow>
-      <BoardRow start={40} end={48} squares={gameState}></BoardRow>
-      <BoardRow start={48} end={56} squares={gameState}></BoardRow>
-      <BoardRow start={56} end={64} squares={gameState}></BoardRow>
+      {/* <div>{playerId}</div> */}
+      <div id="playChessboard">
+        <BoardRow start={0} end={8} squares={gameState}></BoardRow>
+        <BoardRow start={8} end={16} squares={gameState}></BoardRow>
+        <BoardRow start={16} end={24} squares={gameState}></BoardRow>
+        <BoardRow start={24} end={32} squares={gameState}></BoardRow>
+        <BoardRow start={32} end={40} squares={gameState}></BoardRow>
+        <BoardRow start={40} end={48} squares={gameState}></BoardRow>
+        <BoardRow start={48} end={56} squares={gameState}></BoardRow>
+        <BoardRow start={56} end={64} squares={gameState}></BoardRow>
+      </div>
     </div>
   );
 }
