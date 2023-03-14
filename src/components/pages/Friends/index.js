3f0355467c0657c3930b47e295716587d43153fc
William <wabarringer@gmail.com>
2023-03-14 14:03:06 -0700

    update

--- src/components/pages/Friends/index.js
+++ src/components/pages/Friends/index.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import "../Friends/style.css";
 import API from "../../../utils/API";
 
 const Friends = ({ userId }) => {
-  console.log(userId);
-
   useEffect(() => {
     const userData = API.getUsersById(userId).then((data) => {
       console.log(data);
+      const friends = data.Friends;
+      console.log(friends);
     });
   }, []);
 
